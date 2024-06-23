146bd425decd87fe438f04433239b6fb2b7883f9
Entro <shubham64negi@gmail.com>
2024-06-23 21:48:31 +0530

    added comments

--- imagekit/image_kit/views.py
+++ imagekit/image_kit/views.py
@@ -9,28 +9,37 @@ def resize_image(request):
     height = int(request.GET.get('h', 0))
     quality = int(request.GET.get('q', 90))
 
+    # checking for an image url
     if not image_url:
         return HttpResponse("Please provide an image URL.", status=400)
 
     try:
+        # downloading image in bytes format
         response = requests.get(image_url)
         response.raise_for_status()
 
+        # opening the image with PIL using BytesIO that creates a file like object in memory 
         image = Image.open(BytesIO(response.content))
 
+        # determining the scaling factor based on the larger dimension
         if width and height:
             if width >= height:
                 scale_factor = width / image.width
             else:
                 scale_factor = height / image.height
-
+            
+            # scaling the image
             new_size = (int(image.width * scale_factor), int(image.height * scale_factor))
             image = image.resize(new_size, Image.HAMMING)
 
+            # calculating the cropping position 
             x = (image.width - width) // 2
             y = (image.height - height) // 2
 
+            # cropping image
             cropped_image = image.crop((x, y, x + width, y + height))
+
+        # if only the width or height dimension is provided, it determines the scaling factor for the other dimension  
 
         elif width:
             new_width = width
@@ -51,6 +60,7 @@ def resize_image(request):
         cropped_image.save(output, format="WebP", lossless=False, quality=quality)
         output.seek(0)
 
+        # return the cropped image data as the response
         return HttpResponse(output.getvalue(), content_type="image/webp")
     except Exception as e:
         return HttpResponse(f"Error: {str(e)}", status=500)
